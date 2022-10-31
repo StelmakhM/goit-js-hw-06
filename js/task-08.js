b428016a3847d3cf5490f0bf7f0968f0a00dc997
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-10-31 23:32:12 +0200

    done 8th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+const inputs = document.querySelectorAll('.login-form input');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+    const formData = {
+        email: email.value,
+        password: password.value
+    }
+
+    inputs.forEach(input => {
+        if (!input.value) {
+            alert('Будь-ласка, заповніть всі поля');
+        }
+    })
+
+    console.log(formData);
+    event.currentTarget.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
