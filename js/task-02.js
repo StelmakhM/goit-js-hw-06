3ef9b598954cd269b3f0e2fd50c7335cfa500420
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-10-31 19:06:05 +0200

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+// Додаємо в дом одразу гроно
+
+const makeListItems = ingredients => {
+  const items = ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item
+  })
+  list.append(...items)
+}
+
+makeListItems(ingredients);
+
+
+
+// Додаємо в ДОМ по одній ягідці
+
+// const makeListItems = ingredients => {
+//   ingredients.forEach(ingredient => {
+//     const item = document.createElement('li');
+//     item.textContent = ingredient;
+//     item.classList.add('item');
+//     list.append(item);
+//   })
+// }
+
+// makeListItems(ingredients);
+
+
+
+
