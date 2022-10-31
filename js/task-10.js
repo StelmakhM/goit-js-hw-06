6e5626cbdace99c720c0395f244d41924b89c8a1
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-01 01:22:01 +0200

    done 10th task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const containerForBoxes = document.querySelector('#boxes');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+
+let boxSize = 30;
+let boxArr = [];
+
+const createBoxes = (event) => {
+  let amountOfBoxes = input.value;
+  for (let i = 0; i < amountOfBoxes; i += 1) {
+    const box = document.createElement('div');
+
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`
+    boxSize += 10;
+
+    boxArr.push(box);
+    containerForBoxes.append(...boxArr);
+  }
+}
+
+
+const destroyBoxes = (event) => {
+  containerForBoxes.innerHTML = '';
+  boxSize = 30;
+  boxArr = [];
+};
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
