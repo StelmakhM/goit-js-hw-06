499b99c17407c480594019428f91ad2d17b3abd7
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-10-31 23:46:25 +0200

    done 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.body;
+const changeColorBtn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+
+const onClickBgChange = (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+};
+
+
+changeColorBtn.addEventListener('click', onClickBgChange);
